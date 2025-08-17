5a8288691cd2cc34c172c4f24ab85a30a3e03801
rheadsz <rheabdsouza@gmail.com>
2025-08-17 14:24:31 -0700

    04-authentication-setup

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+const Page = () => {
+    console.log("Sign In page");
+    return <SignInView/>
+}
+
+export default Page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/components/ui/card";
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const Page = () => {
+    console.log("Sign Up page");
+    return <SignUpView />;
+}
+
+export default Page;
+
+
