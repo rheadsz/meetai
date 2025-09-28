4d1a0b2918d2e25b7e47409cc5b8f5a4e8dc7c2d
rheadsz <rheabdsouza@gmail.com>
2025-09-27 17:41:38 -0700

    Added dashboard

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,14 @@ import { drizzleAdapter} from "better-auth/adapters/drizzle";
 import {db} from "@/db";
 import * as schema from "@/db/schema";
 export const auth = betterAuth({
+    trustedOrigins: [
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:3002",
+        "http://10.0.0.80:3000",
+        "http://10.0.0.80:3001",
+        "http://10.0.0.80:3002"
+    ],
     socialProviders: {
         github: { 
             clientId: process.env.GITHUB_CLIENT_ID as string, 
